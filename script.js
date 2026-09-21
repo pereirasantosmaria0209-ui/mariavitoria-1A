a942754b2dd3af7ccdbe2e616c6e9bf7824080cd
pereirasantosmaria0209-ui <pereira.santos.maria0209@escola.pr.gov.br>
2026-09-21 09:10:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+ const botoes= document.querSelector("button");
+  botoes.forEach(funcion(botao){
+    let curtiu = false;}
+   {
+    botao.addEvertListener("click",botaoClicado);
+  function botaoClicado() {
+   console.log("fui clicado");
+  let texto = botao.querySelector("span");
+    if(curtiu ===false) {
+  texto.textContent++;
+  }
+   });
